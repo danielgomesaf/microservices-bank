4b396cf76d2cab5259c456dee2d6fbd92d8c7577
Daniel Gomes <danielgomesaf19@gmail.com>
2022-03-08 17:28:15 -0300

    first commit of the eureka server project

---
+++ eurekaserver/src/test/java/com/api/bankaccount/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.bankaccount.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
