bb2cc60f7744440ddb16ddaf89148437adf172a4
Daniel Gomes <danielgomesaf19@gmail.com>
2022-03-08 17:54:19 -0300

    enabling microserver eurekaserver to act as a Service Discovery Agent

--- eurekaserver/src/main/java/com/api/bankaccount/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/com/api/bankaccount/eurekaserver/EurekaserverApplication.java
@@ -2,8 +2,15 @@ package com.api.bankaccount.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+/*
+ * Enables this microservice to act as a Service Discovery Agent by
+ * using a Eureka Server component inside it
+ * 
+ */
+@EnableEurekaServer
 public class EurekaserverApplication {
 
 	public static void main(String[] args) {
