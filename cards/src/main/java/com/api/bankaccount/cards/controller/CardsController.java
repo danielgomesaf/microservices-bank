ef4dab4af2c18ef39620499db05b0a74b183f1d7
Daniel Gomes <danielgomesaf19@gmail.com>
2022-03-17 18:35:13 -0300

    adding correlation tracing id as request header attribute for cards and loans controller methods

--- cards/src/main/java/com/api/bankaccount/cards/controller/CardsController.java
+++ cards/src/main/java/com/api/bankaccount/cards/controller/CardsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.bankaccount.cards.config.CardsServiceConfig;
@@ -27,7 +28,7 @@ public class CardsController {
 	private CardsServiceConfig cardsConfig;
 	
 	@PostMapping("/my-cards")
-	public List<Cards> getCardsDetails(@RequestBody Customer customer) {
+	public List<Cards> getCardsDetails(@RequestHeader("bank-correlation-id") String correlationId, @RequestBody Customer customer) {
 		
 		List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
 		
--- loans/src/main/java/com/api/bankaccount/loans/controller/LoansController.java
+++ loans/src/main/java/com/api/bankaccount/loans/controller/LoansController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.bankaccount.loans.config.LoansServiceConfig;
@@ -27,7 +28,7 @@ public class LoansController {
 	private LoansServiceConfig loansConfig;
 	
 	@PostMapping("/my-loans")
-	public List<Loans> getLoansDetails(@RequestBody Customer customer) {
+	public List<Loans> getLoansDetails(@RequestHeader("bank-correlation-id") String correlationId, @RequestBody Customer customer) {
 		
 		System.out.println("INVOKING LOANS MICROSERVICE. TESTING RETRY PATTERN");
 		
